ab325e062deb6fc3ca9d0c7cc3377558372d8fca
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-01-13 11:14:49 +0000

    Use drupal queues

--- api.php
+++ api.php
@@ -50,12 +50,12 @@ function my_module_foo_list() {
 
 function movie_catalog_add_movie() {
   $movie_data = endpoint_request_data();
-
   if (isset($movie_data->imdb_id)) {
     $imdb_id = check_plain($movie_data->imdb_id);
   }
   else {
     watchdog('error', 'Invalid data passed to end point', array(), WATCHDOG_ALERT);
+    return array('error' => 'Invalid IMDB ID');
   }
 
   // Check if entry already exists.
@@ -67,55 +67,24 @@ function movie_catalog_add_movie() {
     ->fetchAssoc();
 
   if ($result !== FALSE) {
-    // Update node.
-    $node = node_load($result['entity_id']);
-  } else {
-    // Create new node.
-    $node = new stdClass();
-    $node->type = 'movies';
-    node_object_prepare($node);
+    $movie_data->entity_id = $result['entity_id'];
+    $op = 'update';
+  }
+  else {
+    $op = 'add';
   }
 
-  // Populate node values.
-  if (isset($movie_data->title)) {
-    $node->title = check_plain($movie_data->title);
-  }
-  $node->language = LANGUAGE_NONE;
-  if (isset($movie_data->plot)) {
-    $node->body[$node->language][0] = movie_catalog_prepare_field_body($movie_data->plot, 'filtered_html');
-  }
-  $node->field_imdb_id[$node->language][0]['value'] = $imdb_id;
-  if (isset($movie_data->rating)) {
-    $node->field_rating[$node->language][0]['value'] = check_plain($movie_data->rating);
-  }
-  // Genres (Autocomplete field).
-  if (isset($movie_data->genres)) {
-    $node->field_genres[$node->language] = movie_catalog_prepare_term_autocomplete('genres', $movie_data->genres);
-  }
-  if (isset($movie_data->cover)) {
-    $node->field_cover_url[$node->language][0]['value'] = check_plain($movie_data->cover);
-  }
-  //$node->field_cover[$node->language] = movie_catalog_prepare_remote_image($movie_data->cover);
-  $node->status = 1;
-  $node->promote = 0;
-  $node->comment = 0;
-
-  // Term reference (taxonomy) field
-  //$node->field_genres[$node->language][]['tid'] = $form_state['values']['a taxonomy term id'];
-
-  // Entity reference field
-  /*
-  $node->field_customer_nid[$node->language][] = array(
-    'target_id' => $form_state['values']['entity id'],
-    'target_type' => 'node',
+  $queue = DrupalQueue::get('movie_catalog');
+  $queue->createQueue();
+  $queue->createItem(
+    array(
+      'op' => $op,
+      'type' => 'movies',
+      'movie' => $movie_data
+    )
   );
-  */
-  // 'node' is default,
 
-  node_submit($node);
-  node_save($node);
-
-  return array('node' => $node);
+  return array('node' => '???');
 }
 
 function my_module_foo_get($id) {
@@ -131,65 +100,4 @@ function my_module_foo_update() {
 function my_module_callback_authorize() {
   return TRUE;
   // ...
-}
-
-/**
- * Helper to prepare value for body.
- *
- * @param $value
- * @param $format_id
- * @return array
- */
-function movie_catalog_prepare_field_body($value, $format_id) {
-  // Clean array.
-  if (is_array($value)) {
-    foreach ($value as $key => $item) {
-      $value[$key] = '<p>' . check_markup($item, $format_id) . '</p>';
-    }
-    $value = implode("\n", $value);
-  } else {
-    $value = check_markup($value, $format_id);
-  }
-
-  return array(
-    'value' => $value,
-    'summary' => text_summary($value),
-    'format' => $format_id,
-  );
-}
-
-/**
- * Helper to populate Tids for multiple values autocomplete term reference field.
- *
- * @param $field_name
- * @param $vocab_name
- * @param $value
- */
-function movie_catalog_prepare_term_autocomplete($vocab_name, $value) {
-  $value = is_array($value)
-    ? implode(',', $value)
-    : $value;
-  $value = check_plain($value);
-
-  $vocabulary = taxonomy_vocabulary_machine_name_load($vocab_name);
-
-  $values = array();
-  foreach (drupal_explode_tags($value) as $term_entry) {
-    // See if the term exists in the chosen vocabulary and return the tid;
-    // otherwise, create a new 'autocreate' term for insert/update.
-    if ($possibilities = taxonomy_term_load_multiple(array(), array('name' => trim($term_entry), 'vid' => $vocabulary->vid))) {
-      $term = array_pop($possibilities);
-    }
-    else {
-      $term = array(
-        'tid' => 'autocreate',
-        'vid' => $vocabulary->vid,
-        'name' => $term_entry,
-        'vocabulary_machine_name' => $vocabulary->machine_name,
-      );
-    }
-    $values[] = (array)$term;
-  }
-
-  return $values;
 }
--- movie_catalog.profile
+++ movie_catalog.profile
@@ -1,6 +1,8 @@
 <?php
 // $Id$
 
+require_once('movie_catalog.module');
+
 /**
  * Implements hook_form_alter().
  */
