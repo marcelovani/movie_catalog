c2238e97872a93ddacc26cb79fcfb399de5a5d28
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-01-14 19:01:01 +0000

    Simplyfied cron logic

--- api.php
+++ api.php
@@ -4,7 +4,7 @@
  */
 
 define('DRUPAL_ROOT', getcwd());
-require_once DRUPAL_ROOT . '/profiles/movie_catalog/modules/contrib/endpoint/includes/router.inc';
+require_once(DRUPAL_ROOT . '/profiles/movie_catalog/modules/contrib/endpoint/includes/router.inc');
 
 $routes = array(
   'api/imdb/movie/add' => array(
@@ -49,29 +49,16 @@ function my_module_foo_list() {
 
 function movie_catalog_add_movie() {
   $movie_data = endpoint_request_data();
-  if (isset($movie_data->imdb_id)) {
-    $imdb_id = check_plain($movie_data->imdb_id);
-  }
-  else {
+  if (!isset($movie_data->imdb_id)) {
     watchdog('error', 'Invalid data passed to end point', array(), WATCHDOG_ALERT);
     return array('error' => 'Invalid IMDB ID');
-  }
-
-  $result = movie_catalog_movie_exists($imdb_id);
-
-  if ($result !== FALSE) {
-    $movie_data->entity_id = $result['entity_id'];
-    $op = 'update';
-  }
-  else {
-    $op = 'add';
   }
 
   $queue = DrupalQueue::get('movie_catalog');
   $queue->createQueue();
   $queue->createItem(
     array(
-      'op' => $op,
+      'op' => 'add',
       'type' => 'movies',
       'movie' => $movie_data
     )
@@ -93,21 +80,4 @@ function my_module_foo_update() {
 function my_module_callback_authorize() {
   return TRUE;
   // ...
-}
-
-/**
- * Helper to check if the IMDB id already exists.
- *
- * @param $imdb_id
- * @return mixed
- */
-function movie_catalog_movie_exists($imdb_id) {
-  $result = db_select('field_data_field_imdb_id', 'i')
-    ->fields('i', array('entity_id'))
-    ->condition('field_imdb_id_value', $imdb_id, '=')
-    ->condition('language', LANGUAGE_NONE, '=')
-    ->execute()
-    ->fetchAssoc();
-
-  return $result;
 }
