962ef20fde561de31f6d847b21462391f5c020fe
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-01-20 12:43:42 +0000

    Updated module url

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,10 @@ set +x
 echo
 echo "[Info] Bootstrap is cloning the repo"
 
-git clone --branch 7.x-1.x https://git.drupal.org/sandbox/marcelovani/2648246.git movie_catalog
+git clone --branch 7.x-1.x https://git.drupal.org/project/movie_catalog.git movie_catalog
 
 echo "[Info] Bootstrap has finished installation."
 echo
-echo To install Distro run 
+echo To install Distro run
 echo $ cd movie_catalog
 echo $ ./install.sh
